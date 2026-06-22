08bab059f34b850fb19e9bc1b733c4490818c348
agamez <renbriggs10@gmail.com>
2026-06-21 18:11:27 -0700

    inlinePeople added

--- popup.tsx
+++ popup.tsx
@@ -9,6 +9,7 @@ function IndexPopup() {
         padding: 16
       }}>
       <h2>
+        hi
         Welcome to your{" "}
         <a href="https://www.plasmo.com" target="_blank">
           Plasmo
